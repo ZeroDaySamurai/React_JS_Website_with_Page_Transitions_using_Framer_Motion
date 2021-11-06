03e89a30b2b5ae948924bd31f08f192c14a0de64
Shakeel Hoosain <62466466+ZeroDaySamurai@users.noreply.github.com>
2021-11-06 06:45:17 +0200

    added global styles and imported it into app.js

--- page_transition_website/src/App.js
+++ page_transition_website/src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 import Home from './pages/Home';
 import Services from './pages/Services';
 import About from './pages/About';
+import GlobalStyle from './globalStyles';
 
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
+      <AnimatePresence>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/services" component={Services} />
       </Switch>
-
+      </AnimatePresence>
     </Router>
   );
 }
